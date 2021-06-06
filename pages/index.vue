ab834c56b2df597d87ab7c01e4b73698d63f9893
John Smith <johnsmith@esi.info>
2021-06-06 18:43:11 +0100

    moar

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-<article class = "p-10 font-sans">
-  
-   <img src="/img/gnostic-logo.svg" class = "" alt = "GNOSTIC LOGIC" />
-  <h1 class = "font-header text-5xl py-10">Web development in the heart of Scotland</h1>
+<article class = "p-10 font-sans" style ="max-width:1000px;margin: 0px auto;">
+    <div class = "flex justify-center	">
+   <img src="/img/gnostic-logo.svg"  style="max-width:500px;" alt = "GNOSTIC LOGIC" />
+    </div>
+  <h1 class = "font-header text-5xl py-10 text-center">Web development in the heart of Scotland</h1>
   <h2 class = "font-header text-3xl">Bespoke solutions</h2>
-  <p>We believe there isnt a one size fits all approach to web development. With 15+ years experience in many technologies we will work hard to find the exact solution for your business. We won't use off-the-shelf templates to design your site: the site will be crafted to meet your needs not someone else's.</p>
+  <p>We believe there isn't a 'one size fits all' approach to web development. With 15+ years experience in many technologies we will work hard to find the exact solution for your business. We won't use off-the-shelf templates to design your site: the site will be crafted to meet your needs not someone else's.</p>
 </article>
 </template>
 
