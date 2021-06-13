2f40cdac806fa669e63b073f80b7f8ed8df67f2e
John Smith <johnsmith@esi.info>
2021-06-13 19:52:09 +0100

    push

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
     </h1>
     </section>
     <div class="-mt-64 md:grid grid-cols-3 gap-10" style = " background-image: linear-gradient(to bottom, rgba(244,247,240,0), rgba(244,247,240,1)); ">
-      <div class="p-10 rounded-md mb-10" style = "background-color:#2c7169;color:white;">
+      <div class="p-10 rounded-md h-full" style = "background-color:#2c7169;color:white;">
         <h2 class="font-header text-4xl pb-2">Bespoke solutions</h2>
         <p class = "text-white">
           We believe there isn't a 'one size fits all' approach to web
@@ -26,13 +26,13 @@
           to meet your needs not someone else's.
         </p>
       </div>
-            <div class="p-10 rounded-md mb-10" style = "background-color:#2c7169;color:white;">
+            <div class="p-10 rounded-md h-full" style = "background-color:#2c7169;color:white;">
         <h2 class="font-header text-4xl pb-2">Full stack development</h2>
         <p class = "text-white">
             We have 15+ years experience developing websites.
         </p>
       </div>
-            <div class="p-10 rounded-md mb-10" style = "background-color:#2c7169;color:white;">
+            <div class="p-10 rounded-md  h-full" style = "background-color:#2c7169;color:white;">
         <h2 class="font-header text-4xl pb-2">Ethical development</h2>
         <p class = "text-white">
         <ul>
@@ -44,6 +44,24 @@
         </p>
       </div>
     </div>
+
+    <section class = "indented">
+      <div class = "max-w-3xl mx-auto py-10 px-10 text-lg">
+      
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ligula venenatis, eleifend justo vitae, blandit lacus. Suspendisse risus erat, rutrum fringilla libero suscipit, eleifend convallis metus. Morbi sed augue aliquet, luctus augue at, lobortis augue. Vestibulum nec elit quis erat tristique vehicula eget vel ante. Curabitur eleifend augue in mi mollis, nec gravida ipsum sollicitudin. Phasellus nibh eros, tincidunt sed fringilla vitae, interdum volutpat lacus. Morbi lacus sapien, pellentesque vel lacus in, scelerisque laoreet erat.
+</p>
+<p>
+Nulla facilisi. Duis iaculis suscipit nunc a auctor. Nam vestibulum augue at ligula congue, cursus congue purus dictum. Curabitur dapibus, magna ut pellentesque consequat, elit velit cursus nulla, at ornare est odio quis massa. Morbi at mi sit amet elit pharetra feugiat eu ac purus. Sed ac tempor justo, quis vehicula libero. Sed et orci elit. Nullam justo diam, ultricies et diam in, sagittis suscipit magna. Aliquam vel commodo lorem. Integer diam elit, faucibus fringilla pulvinar auctor, gravida sit amet tortor. Etiam varius sodales consectetur. Sed placerat feugiat orci, ut maximus nisi molestie eget. Nullam cursus ultrices nibh et fringilla. Donec id tellus imperdiet, molestie tellus eu, interdum turpis. Nulla commodo sodales lacinia. Etiam ut pellentesque dolor, vel placerat urna.
+</p>
+<p>
+Nulla a magna ac lectus mattis consectetur in porttitor ex. Phasellus vel justo vehicula, posuere dui vitae, placerat mi. Ut non ipsum iaculis, tristique risus et, hendrerit nibh. Vivamus sit amet euismod ipsum. Sed pretium sem at nisi ornare dapibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce eget cursus massa. Sed condimentum ante in dui volutpat molestie. Quisque pretium quam a tellus fermentum, ut commodo diam tempus. Phasellus maximus bibendum est eget malesuada.
+</p>
+<p>
+Quisque felis augue, consequat ac varius eget, eleifend eget nunc. Proin et est eget risus ornare sodales rutrum id orci. Aenean tempus, lorem at condimentum congue, diam nulla lacinia ex, ac ultricies metus nisi nec quam. Etiam ac massa id nibh cursus iaculis quis et felis. Morbi nisl risus, blandit sit amet feugiat ut, aliquet at elit. Proin efficitur in mi ac accumsan. Fusce tellus velit, rhoncus vel tempus a, mollis sit amet nunc. In non massa ante.
+</p>
+
+      </div></section>
     </article>
   </main>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <header>
+      <div class="flex header-wrapper justify-center pb-48 pt-10">
+        <div class="logo-wrapper">
+          <img src="/img/logoalt.svg" alt="GNOSTIC LOGIC" />
+        </div>
+      </div>
+
+    </header>
+    <article class="font-sans container mx-auto" style = "margin-top:-50px;">
+
+
+
+    </article>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.header-wrapper {
+position: relative;
+  width: 100%;
+  background-image: url('/img/texture.svg');
+}
+.header-wrapper::after{
+    position: absolute;
+    bottom:0px;
+    left:0px;
+    right:0px;
+    top:0px;
+    background-size: cover;
+    background-image: linear-gradient(
+    to bottom,
+    rgba(231, 238, 237, 0),
+    rgba(231, 238, 237, 1)
+  );
+}
+
+header .logo-wrapper {
+  width: 500px;
+  img {
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
